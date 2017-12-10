4d755bfa42d6fda451eaf2be46cee4030259a235
XTutorialsOfficial <xtutorials161@gmail.com>
2017-12-10 11:55:14 +0200

    hi6210sft: push device block

---
+++ init/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := system/core/init
+LOCAL_SRC_FILES := init_alice.cpp
+LOCAL_MODULE := libinit_hi6210sft
+
+include $(BUILD_STATIC_LIBRARY)
